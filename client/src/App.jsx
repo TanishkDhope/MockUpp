77e3568ea296f8cf01ba2cdd169c058775968482
Tanishk Dhope <tanishkdhope@gmail.com>
2025-03-09 19:18:47 +0530

    initital commit

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import {Routes, Route} from 'react-router-dom'
+import Login from '../pages/Login'
+import SignUp from '../pages/SignUp'
+
+function App() {
+
+  return (
+    <Routes>
+     <Route path="/" element={<div>Home</div>} />
+     <Route path="/login" element={<Login />} />
+     <Route path="/signup" element={<SignUp />} />
+    </Routes>
+  )
+}
+
+export default App

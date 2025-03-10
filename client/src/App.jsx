7f8e7557e3da0887ce7d9672c5836db6034f19c1
Titan007 <sanskargambhir93@gmail.com>
2025-03-10 21:02:15 +0530

    Shadcn Setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css'
 import {Routes, Route} from 'react-router-dom'
-import Login from '../pages/Login'
-import SignUp from '../pages/SignUp'
+import Login from './pages/Login'
+import SignUp from './pages/SignUp'
+import HomePage from './pages/HomePage'
 
 function App() {
 
   return (
     <Routes>
-     <Route path="/" element={<div>Home</div>} />
+     <Route path="/" element={<HomePage />} />
      <Route path="/login" element={<Login />} />
      <Route path="/signup" element={<SignUp />} />
     </Routes>

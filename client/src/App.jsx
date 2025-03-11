8f30a9bdf0545a60ac292646c5e2fbdcd6e8b535
Tanishk Dhope <tanishkdhope@gmail.com>
2025-03-11 21:14:37 +0530

    vercel.json added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,5 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
-import SignUp from "./pages/SignUp";
 import HomePage from "./pages/HomePage";
 import ProductsPage from "./pages/products/ProductPage";
 import MyScene from "./pages/MyScene";
@@ -11,8 +9,6 @@ function App() {
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/products" element={<ProductsPage />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<SignUp />} />
       <Route path="/myScene" element={<MyScene />} />
     </Routes>
   );
